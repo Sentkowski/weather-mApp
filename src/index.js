0e17d594c14bfbe4dbe8b7f4f2dee9a013784145
Szymon <39162021+Sentkowski@users.noreply.github.com>
2019-08-17 23:41:10 +0200

    Setup API and basic layout

--- src/index.js
+++ src/index.js
@@ -2,23 +2,53 @@
 // jQuery is provided by the script link in html
 import "./styles.scss";
 import "./jquery.vmap.min.exec";
-import "./jquery.vmap.europe.exec";
+import "./jquery.vmap.world.exec";
+import countryCodes from "./country-codes.json";
+import countryCapitals from "./country-capitals.json";
+
+const key = "0a4e47fd599ce51c32367b1db274e6b6";
 
 jQuery(document).ready(function() {
   jQuery("#vmap").vectorMap({
-    map: "europe_en",
+    map: "world_en",
     backgroundColor: "#fffff",
-    borderColor: "#818181",
+    borderColor: "#ffffff",
     borderOpacity: 0.25,
     borderWidth: 1,
-    color: "#aaaaaa",
+    color: "#ffffffaa",
     enableZoom: false,
-    hoverColor: "#bbbbbb",
+    hoverColor: "#ffffffdd",
     hoverOpacity: null,
     normalizeFunction: "linear",
     scaleColors: ["#b6d6ff", "#005ace"],
-    selectedColor: "red",
+    selectedColor: "#ffffff",
     selectedRegions: null,
-    showTooltip: true
+    showTooltip: true,
+    onRegionSelect: handleRegionSelect
   });
 });
+
+async function handleRegionSelect(e, code) {
+  try {
+    const weather = await getWeatherForCapital(code);
+  } catch (e) {
+    console.log("BOOOM");
+  }
+}
+
+async function getWeatherForCapital(countryCode) {
+  const capital = codeToCapital(countryCode);
+  const weatherReq = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${capital}&APPID=${key}`
+  );
+  if (!weatherReq.ok) throw new Error(response.statusText);
+  return await weatherReq.json();
+}
+
+function codeToCapital(code) {
+  const country = countryCodes.find(
+    countryData => countryData.abbreviation.toLowerCase() === code
+  ).country;
+  return countryCapitals.find(countryData => countryData.country === country)
+    .city;
+}
