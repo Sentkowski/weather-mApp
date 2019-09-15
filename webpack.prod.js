8da42c28395ede6647f04471ca4ed21f154bcf12
Szymon <39162021+Sentkowski@users.noreply.github.com>
2019-09-15 18:41:15 +0200

    Add a prefixer

--- webpack.prod.js
+++ webpack.prod.js
@@ -56,6 +56,9 @@ module.exports = {
             loader: "resolve-url-loader"
           },
           {
+            loader: "postcss-loader"
+          },
+          {
             loader: "sass-loader",
             options: {
               sourceMap: true,
